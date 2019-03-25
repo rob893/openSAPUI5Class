7504d3cbc916544f64182a0b9a3b6aa7dfbafd3e
Robert Herber <rherber@nextcenturi.com>
2019-03-25 16:26:50 +0000

    More of the class.

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,12 +1,22 @@
 sap.ui.define(
 	[
 		"sap/ui/core/mvc/Controller",
-		"sap/m/MessageToast"
-	], function(Controller, MessageToast) {
+		"sap/m/MessageToast",
+		"opensap/myapp/model/formatter"
+	], function(Controller, MessageToast, formatter) {
 	
 	Controller.extend("opensap.myapp.controller.App", {
+		
+		formatter : formatter,
+		
 		onShowHello: function() {
-			MessageToast.show("Hello openSAP!");
+			// read msg from i18n model
+			var oBundle = this.getView().getModel("i18n").getResourceBundle();
+			var sRecipient = this.getView().getModel("helloPanel").getProperty("/recipient/name");
+			var sMsg = oBundle.getText("helloMsg", [sRecipient]);
+		
+			// show message
+			MessageToast.show(sMsg);
 		}	
 	});
 });
